fb4175231cdf18344c5512e4d7a1da501aba3b67
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-03-11 22:08:00 +0700

    Update main.cpp
    "endl" instead "\n"

--- 120 дней до успеха/Говнокод/02_lr2/main.cpp
+++ 120 дней до успеха/Говнокод/02_lr2/main.cpp
@@ -1,20 +1,21 @@
 /* Вычисление y=f(x) для заданного x по двум формулам */
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main()
 {
     const float N = 5.329142;
     float x, y;
-    cout << "Calculating y=f(x) for a given x using two formulas\n" ;
-    cout << "Calculating y using the first formula 0<x<=4\n";
-    cout << "Enter  x (0<x<=4)\n";
+    cout << "Calculating y=f(x) for a given x using two formulas" << endl;
+    cout << "Calculating y using the first formula 0<x<=4" << endl;
+    cout << "Enter  x (0<x<=4)" << endl;
     cin >> x;
-    y = N * fabs(M_PI - x) + N / (sqrt(x) + M_PI * x *x);
+    y = N * fabs(M_PI - x) + N / (sqrt(x) + M_PI * x * x);
     cout << "For x=" << x << " y=" << y << endl;
-    cout << "Calculating y using the second formula 4<x<=5.8\n";
-    cout << "Enter  x (4<x<=5.8)\n";
+    cout << "Calculating y using the second formula 4<x<=5.8" << endl;
+    cout << "Enter  x (4<x<=5.8)" << endl;
     cin >> x;
     y = (N - exp(x - M_PI) + pow(cos(x), 2)) / log(x);
     cout << "For x=" << x << " y=" << y;
