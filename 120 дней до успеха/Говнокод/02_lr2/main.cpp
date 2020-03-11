1ebbecb9b276eba1e623ffa24d943e46e9bde30d
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-03-11 21:29:17 +0700

    Update main.cpp
    Replacing PI

--- 120 дней до успеха/Говнокод/02_lr2/main.cpp
+++ 120 дней до успеха/Говнокод/02_lr2/main.cpp
@@ -6,17 +6,16 @@ using namespace std;
 int main()
 {
     const float N = 5.329142;
-    const float PI = 3.14;
     float x, y;
     cout << "Calculating y=f(x) for a given x using two formulas\n" ;
     cout << "Calculating y using the first formula 0<x<=4\n";
     cout << "Enter  x (0<x<=4)\n";
     cin >> x;
-    y = N * fabs(PI - x) + N / (sqrt(x) + PI * x *x);
+    y = N * fabs(M_PI - x) + N / (sqrt(x) + M_PI * x *x);
     cout << "For x=" << x << " y=" << y << endl;
     cout << "Calculating y using the second formula 4<x<=5.8\n";
     cout << "Enter  x (4<x<=5.8)\n";
     cin >> x;
-    y = (N - exp(x - PI) + pow(cos(x), 2)) / log(x);
+    y = (N - exp(x - M_PI) + pow(cos(x), 2)) / log(x);
     cout << "For x=" << x << " y=" << y;
 }
