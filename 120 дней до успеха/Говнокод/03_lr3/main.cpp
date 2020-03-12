f7e5832fa7546e8393a049b1ccddd757fbfa77c1
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-03-12 20:54:40 +0700

    Update main.cpp

--- 120 дней до успеха/Говнокод/03_lr3/main.cpp
+++ 120 дней до успеха/Говнокод/03_lr3/main.cpp
@@ -11,12 +11,14 @@ int main()
     cout << "Calculating y=f(x) for a given x using two formulas" << endl;
     cout << "Enter x" << endl;
     cin >> x;
-    if ( x > 0 && x <= 0.7 ) {
+    if (x > 0 && x <= 0.7) {
         y = N * pow(x + 1, 1 / 3) + pow(M_PI - x, 2);
         cout << "For " << x << " y=" << y;
-    } else
-        if ( x > 0.7 && x < 1) {
+    } else {
+        if (x > 0.7 && x < 1) {
             y = (exp(M_PI + x) + N * sin(x) * sin(x)) / log(x + N);
             cout << "For " << x << " y=" << y;
-        } else cout << "For " << x << " no decision";
+        } else { cout << "For " << x << " no decision";
+        }
+    }
 }
