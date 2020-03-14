8797bc1723b18197e97f8d8832c82d2d7294cf71
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-03-14 17:03:35 +0700

    Update main.cpp

--- 120 дней до успеха/Говнокод/05_lr5/main.cpp
+++ 120 дней до успеха/Говнокод/05_lr5/main.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main() {
-    int c1, c2, c3, c4, x;
+    int firstdigit, seconddigit, thirddigit, fourthdigit, number;
     cout << "The program checks whether a four digit number is a palindrome" << endl;
     cout << "Enter a four digit number ";
-    cin >> x;
-    c1 = x / 1000;
-    c2 = x/ 100 % 10;
-    c3 = x / 10 % 10;
-    c4 = x % 10;
-    if (c1 == c4 && c2 == c3) {
+    cin >> number;
+    firstdigit = number / 1000;
+    seconddigit = number / 100 % 10;
+    thirddigit = number / 10 % 10;
+    fourthdigit = number % 10;
+    if (firstdigit == fourthdigit && seconddigit == thirddigit) {
         cout << "The number is a palindrome";
     } else {
         cout << "The number is not a palindrome";
-        }
+    }
 }
