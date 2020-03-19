9f9f13a4ba74d51a18685df0170ecf8d66f3fbe8
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-03-19 22:47:18 +0700

    update main.cpp

--- 120 дней до успеха/Говнокод/09_lr8/main.cpp
+++ 120 дней до успеха/Говнокод/09_lr8/main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <conio.h>
 
 using namespace std;
 
@@ -17,65 +19,82 @@ int main() {
         int year; //год выпуска
         int power; //мощность
     } TAuto;
-    const int N = 2;
-    int countrecord;
-    TAuto Auto[N];
+    const int N = 100; //размер массива с запасом
+    int countrecord = 0; //число записей в файле
+    map <string, int> book; //ассоциативный массив
+    TAuto Auto[N]; // массив структур, возможно его лучше вставить после чтения из файла и не делать ему размер 100
     TAuto B; //вспомогательная переменная для сортировки
-    ifstream Fin;
-    ofstream Fout;
-    FILE *F;
-    string s;
 
-    //читаем файл
-
-    Fin.open("input_car.dat", ios::binary);
-    Fin.read((char*) Auto, sizeof(TAuto) * N);
-    countrecord = Fin.gcount() / sizeof(TAuto);
-    Fin.close();
-    //F = fopen("input_car.dat", "rb");
-    //countrecord = fread(&Auto[0], sizeof(TAuto), N, F);
-    //fclose(F);
+    //Читаем файл
+    ifstream ifs("input.txt");
+    if (!ifs.is_open()) {
+        cout << "Error. File not open";
+        exit(1);
+    } else {
+        while (!ifs.eof()) {            
+            ifs >> Auto[countrecord].number;
+            ifs >> Auto[countrecord].family;
+            ifs >> Auto[countrecord].model;
+            ifs >> Auto[countrecord].year;
+            ifs >> Auto[countrecord].power;
+            countrecord++;
+        }
+    }    
 
     //Вывод структуры
     cout << "The available data about automobiles" << endl;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < countrecord; i++) {
         cout << Auto[i].number << "  ";
         cout << Auto[i].family << "  ";
         cout << Auto[i].model << "  ";
         cout << Auto[i].year << "  ";
         cout << Auto[i].power << endl;
     }
-/*
-    //Ввод структуры с клавиатуры
-    cout << "Enter your automobile information" << endl << "Enter the car number, last name, model, year of manufacture and power" << endl;
-    for (int i = 0; i < N; i++) {
-        cout << "Enter information about " << i + 1 << " automobile" << endl;
-        cin >> Auto[i].number;
-        cin >> Auto[i].family;
-        cin >> Auto[i].model;
-        cin >> Auto[i].year;
-        cin >> Auto[i].power;
-    }*/
+    cout << endl;
 
-   /* //Сортировка структуры
-    for (int i = 0; i < N - 1; i++) {
-        for (int j = N - 2; j >= i; j--) {
+   //Сортировка и вывод структуры
+    for (int i = 0; i < countrecord - 1; i++) {
+        for (int j = countrecord - 2; j >= i; j--) {
             if (Auto[j].number > Auto[j + 1].number) {
                 B.number = Auto[j].number;
                 Auto[j].number = Auto[j + 1].number;
                 Auto[j + 1].number = B.number;
             }
         }
-    }*/
+    }
+    cout << "Entries are sorted by number. Look output.txt" << endl;
+    cout << "Display entries on the screen? Press 'Enter' if 'YES' or any key if 'NO' " << endl;
+    if (getch() == 13) {
+        cout << "The available data about automobiles" << endl;
+        for (int i = 0; i < countrecord; i++) {
+            cout << Auto[i].number << "  ";
+            cout << Auto[i].family << "  ";
+            cout << Auto[i].model << "  ";
+            cout << Auto[i].year << "  ";
+            cout << Auto[i].power << endl;
+        }
+        cout << endl;
+    }
+
+    //Количество автомобилей каждой марки
+    for (int i = 0; i < countrecord; i++) {
+        int p;
+        p = book.count(Auto[i].model);
+        if (p == 1) {
+            book[Auto[i].model]++;
+        } else {
+            book.insert ( pair<string,int> (Auto[i].model, 1) );
+        }
+    }
+    map <string,int>::iterator it;
+    for ( it=book.begin(); it!=book.end(); it++ ) {
+        cout << it->first << ": " << it->second << endl;
+    }
 
     //вывод в файл
-
-    //F = fopen("output_car.dat", "wb");
-    //fwrite(&Auto[0], sizeof (TAuto), N, F);
-    //fclose(F);
-    Fout.open("output_car.dat", ios::binary);
-    Fout.write((char*) Auto, sizeof(TAuto)*N);
-    Fout.close();
-
-
+    ofstream ofs;
+    ofs.open("output.txt");
+    for (int i = 0; i < countrecord; i++) {
+        ofs << Auto[i].number << " " << Auto[i].family << " " << Auto[i].model << " " << Auto[i].year << " " << Auto[i].power << endl;
+    }
 }
