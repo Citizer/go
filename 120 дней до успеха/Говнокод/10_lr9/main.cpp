25a6c63b51fb01576320153cda0023bc54996de3
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-03-21 09:07:40 +0700

    update main.cpp

--- 120 дней до успеха/Говнокод/10_lr9/main.cpp
+++ 120 дней до успеха/Говнокод/10_lr9/main.cpp
@@ -20,92 +20,92 @@ int main() {
         string author;
         string title;        
         string type; // book или journal
+        int year;
         union {
             char city[50];            
             char jtitle[50];            
         } property1;
         union {
-            char publish[50];
-            int year;
+            char publish[50];            
         } property2;
-        union {
-            int year;
+        union {            
             int number;
         } property3;
     } TBook;
-    const int N = 100;
-    int countrecord = 0, t;
-    TBook Book[N];
-    int mas[N];
+
+    int countrecord, t, i = 0;
 
     //Читаем файл
+    //сначала узнаем количество записей
     ifstream ifs("input.txt");
-        if (!ifs.is_open()) {
-            cout << "Error. File not open";
-            exit(1);
-        } else {
-            while (!ifs.eof()) {
-                ifs >> Book[countrecord].author;
-                ifs >> Book[countrecord].title;                
-                ifs >> Book[countrecord].type;
-                if (Book[countrecord].type == "book") {
-                    ifs >> Book[countrecord].property1.city;
-                    ifs >> Book[countrecord].property2.publish;
-                    ifs >> Book[countrecord].property3.year;
-                } else {
-                    ifs >> Book[countrecord].property1.jtitle;
-                    ifs >> Book[countrecord].property2.year;
-                    ifs >> Book[countrecord].property3.number;
-                }
-                countrecord++;
+    if (!ifs.is_open()) {
+        cout << "Error. File not open";
+        exit(1);
+    } else {
+        ifs >> countrecord;
+        TBook Book[countrecord];
+        while (!ifs.eof()) {
+            ifs >> Book[i].author;
+            ifs >> Book[i].title;
+            ifs >> Book[i].type;
+            ifs >> Book[i].year;
+            if (Book[i].type == "book") {
+                ifs >> Book[i].property1.city;
+                ifs >> Book[i].property2.publish;
+            } else {
+                ifs >> Book[i].property1.jtitle;
+                ifs >> Book[i].property3.number;
             }
+            i++;
         }
 
-    //Вывод структтуры
-    cout << "The available data about books:" << endl;
-    for (int i = 0; i < countrecord; i++) {
-        cout << Book[i].author << "  ";
-        cout << Book[i].title << "  ";
-        cout << Book[i].type << "  ";
-        if (Book[i].type == "book") {
-            cout << Book[i].property1.city << " ";
-            cout << Book[i].property2.publish << " ";
-            cout << Book[i].property3.year << "  ";
+
+        //Вывод структтуры
+        cout << "The available data about books:" << endl;
+        for (i = 0; i < countrecord; i++) {
+            cout << Book[i].author << "  ";
+            cout << Book[i].title << "  ";
+            cout << Book[i].type << "  ";
+            cout << Book[i].year << " ";
+            if (Book[i].type == "book") {
+                cout << Book[i].property1.city << " ";
+                cout << Book[i].property2.publish << " ";
             } else {
-                cout << Book[i].property1.jtitle << " ";
-                cout << Book[i].property2.year << "  ";
+                cout << Book[i].property1.jtitle << " ";                
                 cout << Book[i].property3.number << " ";
             }
+            cout << endl;
+        }
         cout << endl;
-    }
-    cout << endl;
 
-    //заполняем массив по порядку
-    for (int i = 0; i < N; i++) {
-        mas[i] = i;
-    }
+        //заполняем массив для сортировки по порядку
+        int mas[countrecord];
+        for (i = 0; i < countrecord; i++) {
+            mas[i] = i;
+        }
 
-    //Сортировка и вывод структуры
-    for (int i = 0; i < countrecord - 1; i++) {
-        for (int j = countrecord - 2; j >= i; j--) {
-            if (Book[mas[j]].author > Book[mas[j + 1]].author) {
-                t = mas[j];
-                mas[j] = mas[j + 1];
-                mas[j + 1] = t;
+        //Сортировка структуры с помощью вспомогательного массива, структуры не перемещаются
+        for (i = 0; i < countrecord - 1; i++) {
+            for (int j = countrecord - 2; j >= i; j--) {
+                if (Book[mas[j]].author > Book[mas[j + 1]].author) {
+                    t = mas[j];
+                    mas[j] = mas[j + 1];
+                    mas[j + 1] = t;
+                }
             }
         }
-    }
-    //Вывод структтуры
-    cout << "The available data about journal:" << endl;
-    for (int i = 0; i < countrecord; i++) {
-        if (Book[mas[i]].type == "journal") {
-            cout << Book[mas[i]].author << "  ";
-            cout << Book[mas[i]].title << "  ";
-            cout << Book[mas[i]].type << "  ";
-            cout << Book[mas[i]].property1.jtitle << " ";
-            cout << Book[mas[i]].property2.year << "  ";
-            cout << Book[mas[i]].property3.number << " ";
-            cout << endl;
+        //Вывод структтуры
+        cout << "The available data about journal:" << endl;
+        for (i = 0; i < countrecord; i++) {
+            if (Book[mas[i]].type == "journal") {
+                cout << Book[mas[i]].author << "  ";
+                cout << Book[mas[i]].title << "  ";
+                cout << Book[mas[i]].type << "  ";
+                cout << Book[mas[i]].year << " ";
+                cout << Book[mas[i]].property1.jtitle << " ";
+                cout << Book[mas[i]].property3.number << " ";
+                cout << endl;
+            }
         }
     }
 }
