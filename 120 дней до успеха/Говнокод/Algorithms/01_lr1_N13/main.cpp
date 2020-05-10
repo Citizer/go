24e3d06116d09376c647224859d7166641d53dd1
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-05-11 01:17:53 +0700

    add algorithms_01_lr1_N13

---
+++ 120 дней до успеха/Говнокод/Algorithms/01_lr1_N13/main.cpp
@@ -0,0 +1,54 @@
+/* Все ненулевые элементы матрицы D(k,l) расположить в начале массива E( k,l ) и
+подсчитать их количество. */
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+typedef int *pInt;
+
+int main() {
+    int countOfRows, countOfColumns, countNoZero = 0;
+    ofstream ofs;
+
+    //Читаем файл
+    ifstream ifs("input.txt");
+    if (!ifs.is_open()) {
+        cout << "Error. File not open";
+        exit(1);
+    } else {
+        ifs >> countOfRows; //считываем количество строк
+        ifs >> countOfColumns; // считываем количество столбцов
+
+        //Cоздаем матрицу
+        int **D = new int *[countOfRows];
+        for (int i = 0; i < countOfRows; i++ ) {
+            D[i] = new int[countOfColumns];
+        }
+
+        //Создаем массив
+        int *E;
+        E = new int[countOfRows * countOfColumns];
+
+        //Считываем данные в матрицу и располагаем ненулевые элементы в массив
+        for (int i = 0; i < countOfRows; i++) {
+            for (int j = 0; j < countOfColumns; j++) {
+                ifs >> D[i][j];
+                if (D[i][j] != 0) {
+                    E[countNoZero] = D[i][j];
+                    countNoZero++;
+                }
+            }
+        }
+
+        //Записываем массив в файл output
+        ofs.open("output.txt");
+        ofs << "Count of non-zero elements: " << countNoZero << endl;
+        for (int i = 0; i < countNoZero; i++) {
+            ofs << E[i] << " ";
+        }
+        ofs.close();
+        cout << "Data was saved successfully";
+    }
+}
