ce0c9d6ebe143da995c67b958965ffd4887b474a
alexch84 <61972452+alexch84@users.noreply.github.com>
2020-05-18 23:34:35 +0700

    Update main.cpp

--- 120 дней до успеха/Говнокод/Algorithms/04_lr4_N13/main.cpp
+++ 120 дней до успеха/Говнокод/Algorithms/04_lr4_N13/main.cpp
@@ -9,9 +9,10 @@ using namespace std;
 int main() {
 
     list<int> myList;
+    list<int> copymyList;
     list<int>::iterator i;
 
-    int tempvalue, countOfList = 0, choiceOfNumber, prev, next;
+    int tempvalue, countOfList = 0, choiceOfNumber, prev, next, j, valueprev, valuenext;
 
     //Читаем файл
     ifstream ifs("input.txt");
@@ -22,6 +23,7 @@ int main() {
         while (!ifs.eof()) {
             ifs >> tempvalue;
             myList.push_back(tempvalue);
+            copymyList.push_back(tempvalue);
             countOfList++;
         }
 
@@ -29,6 +31,7 @@ int main() {
         for(i = myList.begin(); i != myList.end(); i++) {
             cout << (*i) << " ";
         }
+        cout << endl << endl;
 
         //Меняем местами элементы
         cout << "Select which list number to swap values for: " << endl;
@@ -40,19 +43,43 @@ int main() {
         } else if ((choiceOfNumber < 1) || (choiceOfNumber > countOfList)) {
             cout << "There is no such list number";
         } else {
-            prev = choiceOfNumber--;
+            prev = choiceOfNumber--;//запомнили позиции
             next = choiceOfNumber++;
 
+
+            cout << endl << endl;
+
+            //Находим значение prev и next
+            for (j = 1; j < prev; j++) {
+                valueprev = copymyList.front();
+                copymyList.pop_front();
+            }
+            copymyList.pop_front();
+            valuenext = copymyList.front();
+
+            cout << "Swapping " << valueprev << " and " << valuenext << endl;
+
+            i = myList.begin();
+            for (j = 1; j < prev - 1; j++) {
+                i++;
+            }
+
+            //Меняем местами значения
+            myList.insert(i, valuenext);
+            myList.erase(i);
+            i++;
+            i++;
+            myList.insert(i, valueprev);
+            myList.erase(i);
+
+            ofstream ofs;
+            ofs.open("output.txt");
+            for(i = myList.begin(); i != myList.end(); i++) {
+                cout << (*i) << " ";
+                ofs << (*i) << " ";
+            }
+            ofs.close();
+            cout << endl << "Data is recorded";
         }
-
-
-
-
-
     }
-
-
-
-
-
 }
